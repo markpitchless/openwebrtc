a89d4d8e99a9b836883e5fe0eac92e5a06631570
Mark Pitchless <markpitchless@gmail.com>
2015-02-19 13:26:10 +0000

    Print and parse candidate utils

--- tests/cocoon_utils.c
+++ tests/cocoon_utils.c
@@ -29,3 +29,117 @@ char* prompt_line(const char *prompt) {
   str = realloc(str, i); // trim memory to the str content size
   return str;
 }
+
+void g_print_candidate(OwrCandidate *candidate)
+{
+    // Unpack the candidate data
+    gchar *ice_ufrag, *ice_password;
+    OwrCandidateType candidate_type;
+    OwrComponentType component_type;
+    OwrTransportType transport_type;
+    gchar *foundation, *address, *related_address;
+    gint port, priority, related_port;
+    gchar *candidate_type_name, *component_type_name, *transport_type_name;
+    g_object_get(candidate,
+            "ufrag", &ice_ufrag,
+            "password", &ice_password,
+            "type", &candidate_type,
+            "component-type", &component_type,
+            "foundation", &foundation,
+            "transport-type", &transport_type,
+            "address", &address,
+            "port", &port,
+            "priority", &priority,
+            "base-address", &related_address,
+            "base-port", &related_port, NULL);
+
+    // Convert the numbered types to strings
+    switch ( candidate_type )
+    {
+        case OWR_CANDIDATE_TYPE_HOST             : candidate_type_name = "host"; break;
+        case OWR_CANDIDATE_TYPE_SERVER_REFLEXIVE : candidate_type_name = "srflx"; break;
+        case OWR_CANDIDATE_TYPE_PEER_REFLEXIVE   : candidate_type_name = "prflx"; break;
+        case OWR_CANDIDATE_TYPE_RELAY            : candidate_type_name = "relay"; break;
+        default: candidate_type_name = "unknown"; break;
+    }
+    switch ( component_type )
+    {
+        case OWR_COMPONENT_TYPE_RTP  : component_type_name = "rtp"; break;
+        case OWR_COMPONENT_TYPE_RTCP : component_type_name = "rtcp"; break;
+        default: component_type_name = "unknown"; break;
+    }
+    gchar *transport = "tcp";
+    switch ( transport_type )
+    {
+        case OWR_TRANSPORT_TYPE_UDP:
+            transport = "udp";
+            transport_type_name = "udp";
+            break;
+        case OWR_TRANSPORT_TYPE_TCP_ACTIVE   : transport_type_name = "active"; break;
+        case OWR_TRANSPORT_TYPE_TCP_PASSIVE  : transport_type_name = "passive"; break;
+        case OWR_TRANSPORT_TYPE_TCP_SO       : transport_type_name = "so"; break;
+        default: transport_type_name = "unknown"; break;
+    }
+    //g_print("candidate: type:%s ufrag:%s icepassword:%s component-type:%s foundation:%s transport_type:%i"
+    //        " address:%s port:%i priority:%i related_address:%s related_port:%i\n",
+    //        candidate_type_name, ice_ufrag, ice_password, component_type_name, foundation, transport_type,
+    //        address, port, priority, related_address, related_port);
+
+    // Output SDP ICE candidate line.
+    g_print("candidate:%s %i %s %i %s %i typ %s",
+            foundation, component_type, transport, priority, address, port, candidate_type_name);
+    if (transport_type != OWR_TRANSPORT_TYPE_UDP) { // tcp
+        g_print(" tcptype %s", transport_type_name);
+    }
+    if (candidate_type == OWR_CANDIDATE_TYPE_SERVER_REFLEXIVE) {
+        g_print(" raddr %s rport %i", related_address, related_port);
+    }
+    g_print(" generation 0\n");
+}
+
+OwrCandidate* sdp_2_candidate(gchar *line)
+{
+    OwrCandidate *remote_candidate;
+    OwrCandidateType candidate_type;
+    OwrComponentType component_type;
+    OwrTransportType transport_type;
+    gchar cand_type[100], foundation[100], transport[100], address[100],
+          tcp_type[100], candidate_type_name[100], r_address[100];
+    gint priority, port, r_port;
+
+    sscanf( line, "candidate:%s %u %s %u %s %u typ %s",
+            foundation, &component_type, transport, &priority, address, &port, candidate_type_name);
+
+    if ( strcmp(candidate_type_name, "host") == 0 )  candidate_type = OWR_CANDIDATE_TYPE_HOST;
+    if ( strcmp(candidate_type_name, "srflx") == 0 ) candidate_type = OWR_CANDIDATE_TYPE_SERVER_REFLEXIVE;
+    if ( strcmp(candidate_type_name, "prflx") == 0 ) candidate_type = OWR_CANDIDATE_TYPE_PEER_REFLEXIVE;
+    if ( strcmp(candidate_type_name, "relay") == 0 ) candidate_type = OWR_CANDIDATE_TYPE_RELAY;
+
+    if ( strcmp(transport, "udp") == 0 )
+        transport_type = OWR_TRANSPORT_TYPE_UDP;
+    else
+        transport_type = OWR_TRANSPORT_TYPE_TCP_ACTIVE;
+    if ( transport_type != OWR_TRANSPORT_TYPE_UDP ) { // tcp
+        sscanf( line, "candidate:* * * * * * typ * tcptype %s", tcp_type);
+        if (!g_strcmp0(tcp_type, "active"))
+            transport_type = OWR_TRANSPORT_TYPE_TCP_ACTIVE;
+        else if (!g_strcmp0(tcp_type, "passive"))
+            transport_type = OWR_TRANSPORT_TYPE_TCP_PASSIVE;
+        else
+            transport_type = OWR_TRANSPORT_TYPE_TCP_SO;
+    }
+    if (candidate_type == OWR_CANDIDATE_TYPE_SERVER_REFLEXIVE) {
+        sscanf( line, "candidate:* * * * * * typ * raddr %s rport %u", r_address, &r_port);
+    }
+
+    remote_candidate = owr_candidate_new(candidate_type, component_type);
+    g_object_set(remote_candidate, "foundation", foundation, NULL);
+    g_object_set(remote_candidate, "transport-type", transport_type, NULL);
+    g_object_set(remote_candidate, "address", address, NULL);
+    g_object_set(remote_candidate, "port", port, NULL);
+    g_object_set(remote_candidate, "priority", priority, NULL);
+
+    g_print("CANDIDATE: foundation:%s component-type:%i transport:%s priority:%i address:%s port:%i typ:%s %i\n", foundation, component_type, transport, priority, address, port, candidate_type_name, candidate_type);
+
+    return remote_candidate;
+}
--- tests/cocoon_utils.h
+++ tests/cocoon_utils.h
@@ -6,6 +6,18 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <glib.h>
+#include "owr.h"
+#include "owr_local.h"
+#include "owr_media_source.h"
+#include "owr_media_renderer.h"
+#include "owr_audio_renderer.h"
+#include "owr_video_renderer.h"
+#include "owr_payload.h"
+#include "owr_audio_payload.h"
+#include "owr_video_payload.h"
+#include "owr_session.h"
+#include "owr_media_session.h"
+#include "owr_transport_agent.h"
 
 /**
  * Ask the user on stdout to enter a line of text (on stdin), reads chars until
@@ -15,5 +27,15 @@
  */
 // http://stackoverflow.com/questions/25233730/dynamically-prompt-for-string-without-knowing-string-size
 char* prompt_line(const char *prompt);
+
+/**
+ * Parse an SDP candidate, single line string, into an OwrCandidate*
+ */
+OwrCandidate* sdp_2_candidate(gchar *line);
+
+/**
+ * Print the given OWR ICE candidate as a sdp line.
+ */
+void g_print_candidate(OwrCandidate *candidate);
 
 #endif /* __COCOON_UTILS_H__ */
