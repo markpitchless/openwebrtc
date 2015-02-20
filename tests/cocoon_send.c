a5ced0a5d5f44b618e7bc1e32b305056a83944b1
Mark Pitchless <markpitchless@gmail.com>
2015-02-20 10:28:00 +0000

    Use the util functions on both ends.

--- tests/cocoon_send.c
+++ tests/cocoon_send.c
@@ -73,69 +73,8 @@ static void got_remote_source(OwrMediaSession *session, OwrMediaSource *source, 
 
 static void got_candidate(OwrMediaSession *session_a, OwrCandidate *candidate, OwrMediaSession *session_b)
 {
-    // Unpack the candidate data
-    gchar *ice_ufrag, *ice_password;
-    OwrCandidateType candidate_type;
-    OwrComponentType component_type;
-    OwrTransportType transport_type;
-    gchar *foundation, *address, *related_address;
-    gint port, priority, related_port;
-    gchar *candidate_type_name, *component_type_name, *transport_type_name;
-    g_object_get(candidate,
-            "ufrag", &ice_ufrag,
-            "password", &ice_password,
-            "type", &candidate_type,
-            "component-type", &component_type,
-            "foundation", &foundation,
-            "transport-type", &transport_type,
-            "address", &address,
-            "port", &port,
-            "priority", &priority,
-            "base-address", &related_address,
-            "base-port", &related_port, NULL);
-
-    // Convert the numbered types to strings
-    switch ( candidate_type )
-    {
-        case OWR_CANDIDATE_TYPE_HOST             : candidate_type_name = "host"; break;
-        case OWR_CANDIDATE_TYPE_SERVER_REFLEXIVE : candidate_type_name = "srflx"; break;
-        case OWR_CANDIDATE_TYPE_PEER_REFLEXIVE   : candidate_type_name = "prflx"; break;
-        case OWR_CANDIDATE_TYPE_RELAY            : candidate_type_name = "relay"; break;
-        default: candidate_type_name = "unknown"; break;
-    }
-    switch ( component_type )
-    {
-        case OWR_COMPONENT_TYPE_RTP  : component_type_name = "rtp"; break;
-        case OWR_COMPONENT_TYPE_RTCP : component_type_name = "rtcp"; break;
-        default: component_type_name = "unknown"; break;
-    }
-    gchar *transport = "tcp";
-    switch ( transport_type )
-    {
-        case OWR_TRANSPORT_TYPE_UDP:
-            transport = "udp";
-            transport_type_name = "udp";
-            break;
-        case OWR_TRANSPORT_TYPE_TCP_ACTIVE   : transport_type_name = "active"; break;
-        case OWR_TRANSPORT_TYPE_TCP_PASSIVE  : transport_type_name = "passive"; break;
-        case OWR_TRANSPORT_TYPE_TCP_SO       : transport_type_name = "so"; break;
-        default: transport_type_name = "unknown"; break;
-    }
-    //g_print("candidate: type:%s ufrag:%s icepassword:%s component-type:%s foundation:%s transport_type:%i"
-    //        " address:%s port:%i priority:%i related_address:%s related_port:%i\n",
-    //        candidate_type_name, ice_ufrag, ice_password, component_type_name, foundation, transport_type,
-    //        address, port, priority, related_address, related_port);
-
-    // Output SDP ICE candidate line.
-    g_print("candidate:%s %i %s %i %s %i typ %s",
-            foundation, component_type, transport, priority, address, port, candidate_type_name);
-    if (transport_type != OWR_TRANSPORT_TYPE_UDP) { // tcp
-        g_print(" tcptype %s", transport_type_name);
-    }
-    if (candidate_type == OWR_CANDIDATE_TYPE_SERVER_REFLEXIVE) {
-        g_print(" raddr %s rport %i", related_address, related_port);
-    }
-    g_print(" generation 0\n");
+    //g_print("got_candidate:");
+    g_print_candidate(candidate);
     //owr_session_add_remote_candidate(OWR_SESSION(session_b), candidate);
 }
 
