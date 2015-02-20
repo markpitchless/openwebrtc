5d127a2ac14ddc386f3c4fe38a96f9c1726c49e9
Mark Pitchless <markpitchless@gmail.com>
2015-02-20 15:32:15 +0000

    Start on sending the offer.

--- tests/cocoon_client_std.c
+++ tests/cocoon_client_std.c
@@ -268,6 +268,189 @@ static void send_answer()
         (GAsyncReadyCallback)answer_sent, NULL);
 }
 
+static void send_offer()
+{
+    g_print("send_offer:\n");
+    JsonBuilder *builder;
+    JsonGenerator *generator;
+    JsonNode *root;
+    GList *media_sessions, *item;
+    GObject *media_session;
+    gchar *media_type, *encoding_name;
+    gboolean rtcp_mux;
+    guint payload_type, clock_rate, channels;
+    gboolean ccm_fir, nack_pli;
+    GList *candidates, *list_item;
+    OwrCandidate *candidate;
+    gchar *ice_ufrag, *ice_password;
+    gchar *fingerprint;
+    gchar *json;
+    gsize json_length;
+    SoupSession *soup_session;
+    SoupMessage *soup_message;
+    gchar *url;
+
+    builder = json_builder_new();
+    generator = json_generator_new();
+    json_builder_begin_object(builder);
+    json_builder_set_member_name(builder, "type");
+    json_builder_add_string_value(builder, "offer");
+    json_builder_set_member_name(builder, "sessionDescription");
+    json_builder_begin_object(builder);
+    json_builder_set_member_name(builder, "mediaDescriptions");
+    json_builder_begin_array(builder);
+
+    media_sessions = g_object_get_data(G_OBJECT(transport_agent), "media-sessions");
+    for (item = media_sessions; item; item = item->next) {
+        media_session = G_OBJECT(item->data);
+        json_builder_begin_object(builder);
+
+        json_builder_set_member_name(builder, "type");
+        media_type = g_object_steal_data(media_session, "media-type");
+        json_builder_add_string_value(builder, media_type);
+
+        json_builder_set_member_name(builder, "rtcp");
+        json_builder_begin_object(builder);
+        json_builder_set_member_name(builder, "mux");
+        g_object_get(media_session, "rtcp-mux", &rtcp_mux, NULL);
+        json_builder_add_boolean_value(builder, rtcp_mux);
+        json_builder_end_object(builder);
+
+        json_builder_set_member_name(builder, "payloads");
+        json_builder_begin_array(builder);
+
+        json_builder_begin_object(builder);
+        json_builder_set_member_name(builder, "encodingName");
+        encoding_name = g_object_steal_data(media_session, "encoding-name");
+        json_builder_add_string_value(builder, encoding_name);
+
+        json_builder_set_member_name(builder, "type");
+        payload_type = GPOINTER_TO_UINT(g_object_steal_data(media_session, "payload-type"));
+        json_builder_add_int_value(builder, payload_type);
+
+        json_builder_set_member_name(builder, "clockRate");
+        clock_rate = GPOINTER_TO_UINT(g_object_steal_data(media_session, "clock-rate"));
+        json_builder_add_int_value(builder, clock_rate);
+
+        if (!g_strcmp0(media_type, "audio")) {
+            json_builder_set_member_name(builder, "channels");
+            channels = GPOINTER_TO_UINT(g_object_steal_data(media_session, "channels"));
+            json_builder_add_int_value(builder, channels);
+        } else if (!g_strcmp0(media_type, "video")) {
+            json_builder_set_member_name(builder, "ccmfir");
+            ccm_fir = GPOINTER_TO_UINT(g_object_steal_data(media_session, "ccm-fir"));
+            json_builder_add_boolean_value(builder, ccm_fir);
+            json_builder_set_member_name(builder, "nackpli");
+            nack_pli = GPOINTER_TO_UINT(g_object_steal_data(media_session, "nack-pli"));
+            json_builder_add_boolean_value(builder, nack_pli);
+        } else
+            g_warn_if_reached();
+
+        json_builder_end_object(builder);
+        json_builder_end_array(builder); /* payloads */
+
+        json_builder_set_member_name(builder, "ice");
+        json_builder_begin_object(builder);
+        candidates = g_object_steal_data(media_session, "local-candidates");
+        candidate = OWR_CANDIDATE(candidates->data);
+        g_object_get(candidate, "ufrag", &ice_ufrag, "password", &ice_password, NULL);
+        json_builder_set_member_name(builder, "ufrag");
+        json_builder_add_string_value(builder, ice_ufrag);
+        json_builder_set_member_name(builder, "password");
+        json_builder_add_string_value(builder, ice_password);
+        json_builder_set_member_name(builder, "candidates");
+        json_builder_begin_array(builder);
+        for (list_item = candidates; list_item; list_item = list_item->next) {
+            OwrCandidateType candidate_type;
+            OwrComponentType component_type;
+            OwrTransportType transport_type;
+            gchar *foundation, *address, *related_address;
+            gint port, priority, related_port;
+            candidate = OWR_CANDIDATE(list_item->data);
+            g_object_get(candidate, "type", &candidate_type, "component-type", &component_type,
+                "foundation", &foundation, "transport-type", &transport_type, "address", &address,
+                "port", &port, "priority", &priority, "base-address", &related_address,
+                "base-port", &related_port, NULL);
+            json_builder_begin_object(builder);
+            json_builder_set_member_name(builder, "foundation");
+            json_builder_add_string_value(builder, foundation);
+            json_builder_set_member_name(builder, "componentId");
+            json_builder_add_int_value(builder, component_type);
+            json_builder_set_member_name(builder, "transport");
+            if (transport_type == OWR_TRANSPORT_TYPE_UDP)
+                json_builder_add_string_value(builder, "UDP");
+            else
+                json_builder_add_string_value(builder, "TCP");
+            json_builder_set_member_name(builder, "priority");
+            json_builder_add_int_value(builder, priority);
+            json_builder_set_member_name(builder, "address");
+            json_builder_add_string_value(builder, address);
+            json_builder_set_member_name(builder, "port");
+            json_builder_add_int_value(builder, port);
+            json_builder_set_member_name(builder, "type");
+            json_builder_add_string_value(builder, candidate_types[candidate_type]);
+            if (candidate_type != OWR_CANDIDATE_TYPE_HOST) {
+                json_builder_set_member_name(builder, "relatedAddress");
+                json_builder_add_string_value(builder, related_address);
+                json_builder_set_member_name(builder, "relatedPort");
+                json_builder_add_int_value(builder, related_port);
+            }
+            if (transport_type != OWR_TRANSPORT_TYPE_UDP) {
+                json_builder_set_member_name(builder, "tcpType");
+                json_builder_add_string_value(builder, tcp_types[transport_type]);
+            }
+            json_builder_end_object(builder);
+            g_free(foundation);
+            g_free(address);
+            g_free(related_address);
+        }
+        g_list_free(candidates);
+        json_builder_end_array(builder); /* candidates */
+        json_builder_end_object(builder); /* ice */
+
+        json_builder_set_member_name(builder, "dtls");
+        json_builder_begin_object(builder);
+        json_builder_set_member_name(builder, "fingerprintHashFunction");
+        json_builder_add_string_value(builder, "sha-256");
+        json_builder_set_member_name(builder, "fingerprint");
+        fingerprint = g_object_steal_data(media_session, "fingerprint");
+        json_builder_add_string_value(builder, fingerprint);
+        json_builder_set_member_name(builder, "setup");
+        json_builder_add_string_value(builder, "actpass");
+        json_builder_end_object(builder); /* dtls */
+
+        json_builder_end_object(builder);
+
+        g_free(fingerprint);
+        g_free(ice_password);
+        g_free(ice_ufrag);
+        g_free(encoding_name);
+        g_free(media_type);
+    }
+    json_builder_end_array(builder); /* mediaDescriptions */
+    json_builder_end_object(builder); /* sessionDescription */
+    json_builder_end_object(builder); /* root */
+
+    json_generator_set_pretty(generator, TRUE);
+    root = json_builder_get_root(builder);
+    json_generator_set_root(generator, root);
+    json = json_generator_to_data(generator, &json_length);
+    json_node_free(root);
+    g_object_unref(builder);
+    g_object_unref(generator);
+
+    g_print("%s\n", json);
+
+    //url = g_strdup_printf(SERVER_URL"/ctos/%s/%u/%s", session_id, client_id, peer_id);
+    //soup_session = soup_session_new();
+    //soup_message = soup_message_new("POST", url);
+    //g_free(url);
+    //soup_message_set_request(soup_message, "application/json",
+    //    SOUP_MEMORY_TAKE, json, json_length);
+    //soup_session_send_async(soup_session, soup_message, NULL,
+    //    (GAsyncReadyCallback)answer_sent, NULL);
+}
+
 static void got_candidate(GObject *media_session, OwrCandidate *candidate, gpointer user_data)
 {
     g_print("got_candidate:\n");
@@ -721,13 +904,14 @@ static void eventsource_request_sent(SoupSession *soup_session, GAsyncResult *re
 static void send_eventsource_request(const gchar *url)
 {
     g_print("send_eventsource_request: %s\n", url);
-    SoupSession *soup_session;
-    SoupMessage *soup_message;
+    // TODO: Set up read of stdin
+    //SoupSession *soup_session;
+    //SoupMessage *soup_message;
 
-    soup_session = soup_session_new();
-    soup_message = soup_message_new("GET", url);
-    soup_session_send_async(soup_session, soup_message, NULL,
-        (GAsyncReadyCallback)eventsource_request_sent, NULL);
+    //soup_session = soup_session_new();
+    //soup_message = soup_message_new("GET", url);
+    //soup_session_send_async(soup_session, soup_message, NULL,
+    //    (GAsyncReadyCallback)eventsource_request_sent, NULL);
 }
 
 static void got_local_sources(GList *sources, gchar *url)
@@ -743,6 +927,12 @@ static void got_local_sources(GList *sources, gchar *url)
     }
 }
 
+static gboolean send_offer_cb(gpointer *user_data)
+{
+    send_offer();
+    return FALSE; // only 1 offer
+}
+
 gint main(gint argc, gchar **argv)
 {
     GMainLoop *main_loop;
@@ -756,12 +946,18 @@ gint main(gint argc, gchar **argv)
 
     session_id = argv[1];
     client_id = g_random_int();
+
+    g_print("event:join\ndata:%i\n:\n", client_id);
+
     url = g_strdup_printf(SERVER_URL"/stoc/%s/%u", session_id, client_id);
     main_loop = g_main_loop_new(NULL, FALSE);
     main_context = g_main_context_default();
     owr_init_with_main_context(main_context);
     owr_get_capture_sources(OWR_MEDIA_TYPE_AUDIO | OWR_MEDIA_TYPE_VIDEO,
         (OwrCaptureSourcesCallback)got_local_sources, url);
+
+    g_timeout_add_seconds(4, (GSourceFunc)send_offer_cb, NULL);
+
     g_main_loop_run(main_loop);
     return 0;
 }
